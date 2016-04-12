c37149aad61720954414341b4f543c7a526858ba
monprin <jeffe174@umn.edu>
2016-04-11 22:23:53 -0500

    Made improvements after some testing

--- cobConda.sh
+++ cobConda.sh
@@ -25,10 +25,10 @@ export PATH=$BASE/.local/bin:$PATH
 #===================================================
 #----------------Install conda ---------------------
 #===================================================
-if ! hash foo 2>/dev/null ; then
+if ! hash conda 2>/dev/null ; then
 	wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-	bash miniconda.sh -b $HOME/miniconda
-	export PATH="$HOME/miniconda/bin:$PATH"
+	bash miniconda.sh -b -f -p $BASE/.local
+	rm -rf miniconda.sh
 fi
 
 
